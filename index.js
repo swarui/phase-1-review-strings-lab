78a1cf32bac9785b98626d73a87177b053f67035
stevewarui,@gmail.com <stevewarum@gmail.com>
2023-09-19 12:29:32 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Steve Maina';
+const welcomeMessage = 'Welcome to Flatbook, + Steve Maina, !';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, STEVE MAINA,  !';
+const shortGreeting = 'Welcome, + S, !'
